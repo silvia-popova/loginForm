d1d85dd89936368f9300e445754d28e6c99718af
Silvia Popova <silvi.1981@windowslive.com>
2016-11-06 23:15:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$(function () {
+    $("#datepicker").datepicker();
+    $('div.ui-datepicker').css({
+   fontSize:'10px',
+   width: '30px'
+});
+
+
+$('.ui-datepicker-title').css({
+   margin:'0px',
+   padding: '0px'
+});
+});
+             
+ 
+
+
